f847667b4f0f62d90cf091c660dcb2c2a9e46e7d
Joeri Gantois <info@JO3RI.be>
2016-12-14 22:55:24 +0100

    changed the logo

--- BGEM_AB/menu.h
+++ BGEM_AB/menu.h
@@ -7,8 +7,7 @@
 void stateMenuIntro()
 {
   globalCounter++;
-  for (byte i = 0; i < 4; i++) sprites.drawSelfMasked(32 * i, 10, TEAMarg, i);
-  sprites.drawSelfMasked(43, 50, TEAM_argPart5, 0);
+  sprites.drawSelfMasked(34, 4, T_arg, 0);
   if (globalCounter > 180)
   {
     globalCounter = 0;
@@ -20,7 +19,7 @@ void stateMenuMain()
 {
   for (byte i = 0; i < 4; i++) sprites.drawSelfMasked(32 * i, 0, splashScreen, i);
   sprites.drawSelfMasked(0, 48, mainMenu, menuSelection - 2);
-  sprites.drawSelfMasked(64, 48, mainMenu, menuSelection - 2+6);
+  sprites.drawSelfMasked(64, 48, mainMenu, menuSelection - 2 + 6);
   if (arduboy.justPressed(RIGHT_BUTTON) && (menuSelection < 5))menuSelection++;
   if (arduboy.justPressed(LEFT_BUTTON) && (menuSelection > 2))menuSelection--;
   if (arduboy.justPressed(A_BUTTON | B_BUTTON)) gameState = menuSelection;
@@ -28,7 +27,7 @@ void stateMenuMain()
 
 void stateMenuHelp()
 {
-  for (byte i = 0; i < 2; i++) sprites.drawSelfMasked(32, 32*i, qrcode, i);
+  for (byte i = 0; i < 2; i++) sprites.drawSelfMasked(32, 32 * i, qrcode, i);
   if (arduboy.justPressed(A_BUTTON | B_BUTTON)) gameState = STATE_MENU_MAIN;
 }
 
